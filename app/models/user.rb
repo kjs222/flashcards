149de67fc3c07110e299d84a82db5f5bb46671d4
Kerry <kerry.sheldon@gmail.com>
2016-07-25 22:05:31 -0600

    fixed validation on password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  has_secure_password
+  has_secure_password :validations => false
 
   def self.from_omniauth(auth_info)
+
     where(uid: auth_info['uid']).first_or_create do |user|
       user.uid = auth_info["uid"]
       user.token = auth_info["credentials"]["token"]
