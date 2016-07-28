9907cf3c48fc90eb814f4c0436821700091df388
Kerry <kerry.sheldon@gmail.com>
2016-07-28 15:44:38 -0600

    activity log functioning

--- spec/features/user_sees_recent_activity_spec.rb
+++ spec/features/user_sees_recent_activity_spec.rb
@@ -2,13 +2,21 @@ require 'rails_helper'
 
 RSpec.feature "User sees recent activity" do
   scenario "user sees activity" do
-    #from dashboard, i click on activity
-    #i visit /activity
-    #i see a list of 20 most recent goal setting, skill adding, and session logging activities in reverse chron order
+    user = User.create(gh_uid: 1, name: "Kerry Sheldon", nickname: "kjs222", gh_token: ENV['GITHUB_TOKEN'], email: "myemail@email.com", quiz_id: "kjs222", quiz_token: ENV['QUIZLET_TOKEN'])
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
+    create_list(:skill, 5)
+    create_list(:current_goal, 5)
+    create_list(:session, 10)
+    create_list(:skill, 5)
 
+    visit dashboard_index_path
+    click_on "Recent Activity"
+    expect(current_path).to eq(activity_index_path)
 
+    expect(page).to have_content("added skill")
+    expect(page).to have_content("completed a 15 minute practice session of for the following skill")
 
   end
-  
+
 
 end
