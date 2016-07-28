466d709771965bbedabb7e840b2bcd470af1f7aa
Kerry <kerry.sheldon@gmail.com>
2016-07-27 23:11:49 -0600

    feature test for show hide terms working

--- spec/features/user_sees_quizlet_terms_spec.rb
+++ spec/features/user_sees_quizlet_terms_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 RSpec.feature "user sees quizlet terms" do
-  scenario "terms are shown when clicked" do
+  scenario "terms are shown when clicked", js: true do
 
      user = User.create(gh_uid: 1, name: "Kerry Sheldon", nickname: "kjs222", gh_token: ENV['GITHUB_TOKEN'], email: "myemail@email.com", quiz_id: "kjs222", quiz_token: ENV['QUIZLET_TOKEN'])
      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
 
-     VCR.use_cassette("my_sets") do
        visit quizlet_index_path
-       expect(page).to_not have_content("second
-       oauth technical step")
-       within("#oauth-technical") do
-         click_on "See Terms"
+
+       within(".technical") do
+         expect(page).to_not have_content("second
+         oauth technical step")
+         click_on "Show Terms"
+         expect(page).to have_content("second
+         oauth technical step")
+         click_on "Hide Terms"
+         expect(page).to_not have_content("second
+         oauth technical step")
        end
-       expect(page).to have_content("second
-       oauth technical step")
-      
-     end
   end
 end
