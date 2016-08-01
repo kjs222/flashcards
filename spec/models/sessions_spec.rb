eeb9ff1efb76e2e676f14b9a405e34d9dc574717
Kerry <kerry.sheldon@gmail.com>
2016-08-01 13:34:36 -0600

    session tests updated

--- spec/models/sessions_spec.rb
+++ spec/models/sessions_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Session, type: :model do
-
-  it "identifies correct sessions for a time period" do
+  #not used anymore
+  xit "identifies correct sessions for a time period" do
     session_1_week_ago = create(:session_1_week_ago)
     session_2_weeks_ago = create(:session_2_weeks_ago)
     session_3_weeks_ago = create(:session_3_weeks_ago)
@@ -27,7 +27,7 @@ RSpec.describe Session, type: :model do
     session2 = skill.sessions.create(duration: 60, created_at: "2016-08-02 00:00:00")
     total_duration = session1.duration + session2.duration
 
-    expect(Session.time_series_session_data(session1.created_at, session1.created_at + 2.days, "day")).to eq([["Aug 01", "Aug 02", "Aug 03"], [session1.duration, session2.duration, 0]])
+    expect(Session.time_series_session_data(user, session1.created_at, session1.created_at + 2.days, "day")).to eq([["Aug 01", "Aug 02", "Aug 03"], [session1.duration, session2.duration, 0]])
   end
 
   it "gets session duration sum by month" do
@@ -37,19 +37,21 @@ RSpec.describe Session, type: :model do
     session2 = skill.sessions.create(duration: 60, created_at: "2016-08-01 00:00:00")
     total_duration = session1.duration + session2.duration
 
-    expect(Session.time_series_session_data(session1.created_at, session1.created_at + 1.days, "month")).to eq([["August 2016"], [total_duration]])
+    expect(Session.time_series_session_data(user, session1.created_at, session1.created_at + 1.days, "month")).to eq([["August 2016"], [total_duration]])
   end
 
   it "gets correct data in right format based on time period" do
-    session_1_week_ago = create(:session_1_week_ago)
-    session_2_weeks_ago = create(:session_2_weeks_ago)
-    session_4_weeks_ago = create(:session_4_weeks_ago)
-    session_2_months_ago = create(:session_2_months_ago)
+    user = create(:user)
+    skill = create(:skill, user: user)
+    session_1_week_ago = create(:session_1_week_ago, skill: skill)
+    session_2_weeks_ago = create(:session_2_weeks_ago, skill: skill)
+    session_4_weeks_ago = create(:session_4_weeks_ago, skill: skill)
+    session_2_months_ago = create(:session_2_months_ago, skill: skill)
 
-    expect(Session.data_for_charts(2).count).to eq(2)
-    expect(Session.data_for_charts(1).first.first.include?("2016")).to be(false)
-    expect(Session.data_for_charts(52).first.first.include?("2015")).to be(true)
-    expect(Session.data_for_charts(52).last.count).to eq(13)
+    expect(Session.data_for_charts(user, 2).count).to eq(2)
+    expect(Session.data_for_charts(user, 1).first.first.include?("2016")).to be(false)
+    expect(Session.data_for_charts(user, 52).first.first.include?("2015")).to be(true)
+    expect(Session.data_for_charts(user, 52).last.count).to eq(13)
 
 
   end
