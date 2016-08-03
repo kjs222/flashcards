109cadeb249943f278cf6b03b37042164ea824db
Kerry <kerry.sheldon@gmail.com>
2016-08-03 17:27:09 -0600

    removed cacheing to allow for better demo

--- app/services/quizlet_service.rb
+++ app/services/quizlet_service.rb
@@ -8,10 +8,10 @@ class QuizletService
   end
 
   def get_sets
-    Rails.cache.fetch("user-#{@user.id}-api-quizsets", expires_in: 10.minutes) do
+    # Rails.cache.fetch("user-#{@user.id}-api-quizsets", expires_in: 10.minutes) do
       response = @connection.get("/2.0/users/#{user.quiz_id}/sets")
       parse(response)
-    end
+    # end
   end
 
 
